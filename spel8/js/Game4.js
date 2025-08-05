4e696f3de3033a2b0fa16f0601d99c0091f892ee
quantumstahl <quantumstahl@gmail.com>
2025-08-05 02:04:07 +0200

    u

--- spel8/js/Game4.js
+++ spel8/js/Game4.js
@@ -195,6 +195,7 @@ class Game4 {
         });
         canvas.addEventListener("touchend", function(e) {
             e.preventDefault();
+            if (window.isPlacingBuilding) return;
             const currentMap = game.maps[game.currentmap];
             const zoomFactor = 1 + (1 * currentMap.zoom / 100);
             if (e.touches.length == 0) {
