c36fc3f80a9ff907db95de4a9e26d30ef88b9957
quantumstahl <quantumstahl@gmail.com>
2025-09-19 12:12:57 +0200

    u

--- paint/sw.js
+++ paint/sw.js
@@ -1,9 +1,13 @@
 // Enkel offline-cache för MaxPaint
 const CACHE_NAME = 'maxpaint-v1';
 const ASSETS = [
-  '/', 'index.html',
-  'styles.css', 'app.js',
-  'icons/icon-192.png', 'icons/icon-512.png','icons/icon-180.png'
+  '/paint/',
+  '/paint/index.html',
+  '/paint/app.js',
+  '/paint/icons/icon-192.png',
+  '/paint/icons/icon-512.png',
+  '/paint/icons/icon-180.png',
+  '/paint/manifest.webmanifest'
 ];
 
 self.addEventListener('install', (e) => {
