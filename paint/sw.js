8dda45c2607167b2a44d4d10a0f81ff6fd825b4a
quantumstahl <quantumstahl@gmail.com>
2025-09-29 21:11:53 +0200

    u

--- paint/sw.js
+++ paint/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1.11b'; // <- this is what you'll show in the badge
+const CACHE_NAME = 'v1.11c'; // <- this is what you'll show in the badge
 const ASSETS = [
   'index.html',
   'app.js',
