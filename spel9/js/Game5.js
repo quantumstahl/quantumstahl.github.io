54df04df02254a92b58a7454b6ccc4c54fc90772
quantumstahl <quantumstahl@gmail.com>
2026-03-15 20:24:59 +0100

    u

--- spel9/js/Game5.js
+++ spel9/js/Game5.js
@@ -2343,42 +2343,7 @@ class Objecttype {
 
 
         const offCanvas =offscreenCanvas;
-        
 
-        if (o.water) {
-               // if (offscreenCanvas.width !== sprite.width||offscreenCanvas.height !== sprite.height) {
-                    offscreenCanvas.width = sprite.width;
-                    offscreenCanvas.height = sprite.height + (3 * 2); // Extra höjd för vågen
-               // }
-                offCtx.clearRect(0, 0, offCanvas.width, offCanvas.height);
-                const slice = 5; // Mycket snyggare med 1px slices när vi kör offscreen
-                const strength = 6;
-                const t = time * 0.003;
-
-                for (let x = 0; x < sprite.width; x += slice) {
-                    const offset = Math.sin((x * 0.05) + t) * strength;
-
-                    // Här ritar vi 1:1. Ingen skalning sker i loopen!
-                    offCtx.drawImage(
-                        sprite,
-                        x, 0, slice, sprite.height,      // source
-                        x, strength + offset, slice, sprite.height // dest (i offscreen canvas)
-                    );
-            
-            
-
-                }
-                offCtx.globalCompositeOperation = "source-atop";
-                offCtx.fillStyle = tint;
-                offCtx.fillRect(0,0,offscreenCanvas.width,offscreenCanvas.height);
-                offCtx.globalCompositeOperation = "source-over";
-                ctx.drawImage(
-                    offCanvas, 
-                    0, 0, sprite.width, sprite.height + (strength * 2), // source (hela buffern)
-                    -w / 2, -h / 2, w, h                          // destination (skalad)
-                );
-            }
-            else {
                /// if (offscreenCanvas.width !== sprite.width||offscreenCanvas.height !== sprite.height) {
                     offscreenCanvas.width = sprite.width;
                     offscreenCanvas.height = sprite.height; // Extra höjd för vågen
@@ -2390,7 +2355,7 @@ class Objecttype {
                 offCtx.fillRect(0,0,offscreenCanvas.width,offscreenCanvas.height);
                 offCtx.globalCompositeOperation = "source-over";
                 ctx.drawImage(offCanvas, -w/2, -h/2,w,h);
-            }
+            
       }
     
     
@@ -2478,65 +2443,8 @@ class Objecttype {
             
 
             if (o.water) {
-                
-                    // Uppdatera storleken om bilden ändras
-               // if (offscreenCanvas.width !== img.width) {
-                    offscreenCanvas.width = img.width;
-                    offscreenCanvas.height = img.height + (3 * 2); // Extra höjd för vågen
-              //  }
-
-                const offCanvas =offscreenCanvas;
-
-                // Rensa offscreen-ytan (viktigt för transparens!)
-                offCtx.clearRect(0, 0, offCanvas.width, offCanvas.height);
-                
-                
-                
-                const slice = 5; // Mycket snyggare med 1px slices när vi kör offscreen
-                const strength = 6;
-                const t = time * 0.003;
-
-                for (let x = 0; x < img.width; x += slice) {
-                    const offset = Math.sin((x * 0.05) + t) * strength;
-
-                    // Här ritar vi 1:1. Ingen skalning sker i loopen!
-                    offCtx.drawImage(
-                        img,
-                        x, 0, slice, img.height,      // source
-                        x, strength + offset, slice, img.height // dest (i offscreen canvas)
-                    );
-            
-            
-
-                }
-                if(o.name=="water"||o.name=="water2"){
-                
-                    offCtx.beginPath();
-                    for (let x = 0; x < img.width; x++) {
-                        const base = Math.sin((x * 0.05) + t) * strength;
-                        const ripple = getRippleOffset(x);
-                        const y = strength + base + ripple+1;
-
-                        if (x === 0) offCtx.moveTo(x, y);
-                        else offCtx.lineTo(x, y);
-                    }
-
-
-                    offCtx.strokeStyle = "white";
-                    offCtx.lineWidth = 2;
-                    offCtx.stroke();
-
-                    offCtx.strokeStyle = "rgba(0,0,0,0.25)";
-                    offCtx.lineWidth = 8;
-                    offCtx.stroke();
-                }
-                // 2. Rita nu hela den färdiga "vågbilden" till huvud-canvasen
-                // Här skalar vi hela bilden på en gång, vilket eliminerar gliporna.
-                ctx.drawImage(
-                    offCanvas, 
-                    0, 0, img.width, img.height + (strength * 2), // source (hela buffern)
-                    -w / 2, -h / 2, w, h                          // destination (skalad)
-                );
+                const bob = Math.sin(time * 0.003) * 3;
+                ctx.drawImage(img, -w/2, -h/2+bob,w,h);
             }
             else ctx.drawImage(img, -w/2, -h/2,w,h);
               
