2a5b3ae0d84c70062c813ee472cb774888662a0e
quantumstahl <quantumstahl@gmail.com>
2026-03-17 13:51:19 +0100

    u

--- spel9/js/joy.js
+++ spel9/js/joy.js
@@ -181,7 +181,7 @@ var JoyStick = (function (container, parameters, callback) {
         updateGeometry();
         updateDefaultCenter();
         
-        if(centerY<300){centerX = defaultCenterX; centerY = defaultCenterY;}
+        if(centerY<300){centerX = defaultCenterX; centerY = defaultCenterY;movedX = centerX;movedY = centerY;}
         
         
 
