a7c26e700b27b0c5f41adb426ccf5c0310989d4e
quantumstahl <quantumstahl@gmail.com>
2026-03-17 13:01:31 +0100

    u

--- spel9/js/joy.js
+++ spel9/js/joy.js
@@ -97,9 +97,9 @@ var JoyStick = (function (container, parameters, callback) {
             centerY = canvas.height * 0.78;
         }
         else{
-            if(centerY!==canvas.height * 0.85){movedX = canvas.width * 0.18;movedY = canvas.height * 0.85;}
-            centerX = canvas.width * 0.18;
-            centerY = canvas.height * 0.85;
+            if(centerY!==canvas.height * 0.80){movedX = canvas.width * 0.20;movedY = canvas.height * 0.80;}
+            centerX = canvas.width * 0.20;
+            centerY = canvas.height * 0.80;
             
         }
         externalRadius = Math.min(canvas.width, canvas.height) * 0.09;
