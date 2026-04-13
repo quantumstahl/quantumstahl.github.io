a8cbad28cf05960b0ca593595360c7a36bdfc25d
quantumstahl <quantumstahl@gmail.com>
2026-04-13 14:05:35 +0200

    Update ClientApp.js

--- spelmultiplayer1/ClientApp.js
+++ spelmultiplayer1/ClientApp.js
@@ -384,7 +384,7 @@ class ClientApp {
     }
 
     handleTouchCommand(worldX, worldY) {
-        //this.leftclicked=true;
+        this.leftclicked=true;
         if(this.game.UISIZE()||this.game.buildMode)return;
         const selected = this.getSelectedEntities();
         if (selected.length === 0) return;
