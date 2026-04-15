2b664aa9eeeab82374288ff8acb189fb236e4947
quantumstahl <quantumstahl@gmail.com>
2026-04-15 20:32:02 +0200

    u

--- spelmultiplayer1/ClientApp.js
+++ spelmultiplayer1/ClientApp.js
@@ -164,8 +164,8 @@ class ClientApp {
     
     
     connect() {
-       // this.ws = new WebSocket("wss://game.quantumstahl.com");
-        this.ws = new WebSocket(`ws://${window.location.hostname}:3000`);
+        this.ws = new WebSocket("wss://game.quantumstahl.com");
+       // this.ws = new WebSocket(`ws://${window.location.hostname}:3000`);
         this.game.setWS(this.ws);
         this.ws.binaryType = "arraybuffer";
 
