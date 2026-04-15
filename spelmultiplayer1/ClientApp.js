72c9a29d1eb024b43ce1bdb3528edac950b9c5cc
quantumstahl <quantumstahl@gmail.com>
2026-04-15 14:48:02 +0200

    Update ClientApp.js

--- spelmultiplayer1/ClientApp.js
+++ spelmultiplayer1/ClientApp.js
@@ -239,7 +239,7 @@ class ClientApp {
     }
 
     updateNetworkRendering() {
-        const renderDelay = 200;
+        const renderDelay = 300;
         const renderTime = performance.now() - renderDelay;
 
         const world = this.game.world;
