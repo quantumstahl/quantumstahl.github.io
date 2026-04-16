d5f883e18b275e3bf4859e8c559ee520d94ebf94
quantumstahl <quantumstahl@gmail.com>
2026-04-16 09:30:52 +0200

    Update ClientApp.js

--- spelmultiplayer1/ClientApp.js
+++ spelmultiplayer1/ClientApp.js
@@ -25,7 +25,7 @@ function mobileAndTabletCheck() {
 
 class ClientApp {
     constructor() {
-        this.debugPanel = new DebugPanel();
+        
         this.ws = null;
         this.game = new GameClient(); 
         this.myId = null;
@@ -169,7 +169,7 @@ class ClientApp {
         this.ws.binaryType = "arraybuffer";
 
         this.ws.onmessage = (event) => {
-            this.debugPanel.onPacket();
+            
             if (typeof event.data === "string") {
                 const data = JSON.parse(event.data);
                 this.handleServerMessage(data);
@@ -593,7 +593,7 @@ update(scale) {
         }
 
         ctx.restore();
-        this.debugPanel.draw(ctx, this.game);
+        
     }
 
     gameLoop(time) {
@@ -606,7 +606,7 @@ update(scale) {
 
         const scale = deltaMs / (1000 / 60);
         
-        this.debugPanel.beginFrame();
+        
         this.update(scale);
         this.draw(scale);
 
