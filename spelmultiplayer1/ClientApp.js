8deac7c02d24d4f7e732af65c002136da2bae453
quantumstahl <quantumstahl@gmail.com>
2026-04-18 16:14:10 +0200

    u

--- spelmultiplayer1/ClientApp.js
+++ spelmultiplayer1/ClientApp.js
@@ -579,7 +579,7 @@ class ClientApp {
         
         const clicked = this.getEntityAt(worldX, worldY);
 
-        if(!(selected[0].type==="sheep" && clicked.type==="sheep")&&(!clicked||(clicked.owner!==this.myId||clicked.type==="sheep")||(clicked.owner===this.myId&&clicked.buildProgress&&clicked.buildProgress<1)))this.sendRightClickCommand(worldX, worldY, clicked ? clicked.id : null);
+        if((!clicked||(clicked.owner!==this.myId||clicked.type==="sheep")||(clicked.owner===this.myId&&clicked.buildProgress&&clicked.buildProgress<1)))if(!clicked||!(selected[0].type==="sheep" && clicked.type==="sheep"))this.sendRightClickCommand(worldX, worldY, clicked ? clicked.id : null);
     
         if(clicked){if(!isbuilding)this.deselectAll();if(clicked.owner===this.myId&&!(isbuilding&&clicked.type==="sheep")&&!(clicked.buildProgress&&clicked.buildProgress<1) ){this.handlePointerLeftDown(worldX, worldY);}}
         
