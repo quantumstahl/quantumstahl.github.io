2d9bba748183c4cdabf1fbfeaa2b0c6a98cae75c
quantumstahl <quantumstahl@gmail.com>
2026-05-03 20:00:07 +0200

    u

--- pant3d/joy.js
+++ pant3d/joy.js
@@ -129,7 +129,7 @@ var JoyStick = (function (container, parameters, callback) {
     function changedir(){
         const isPortrait = canvas.height > canvas.width;
         
-        if(isPortrait!==savedisportrait||counter>0){if(counter==0)counter=3;savedisportrait=isPortrait;counter--;return true;}
+        if(isPortrait!==savedisportrait||counter>0){if(counter==0)counter=10;savedisportrait=isPortrait;counter--;return true;}
         
         return false;
         
